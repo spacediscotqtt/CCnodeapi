66173a92281ea1b7ed82bda51a6ad10b84cc0941
Timothy tran <s3540567@student.rmit.edu.au>
2022-04-22 18:02:06 +1000

    codedeploy

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth");
+
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
@@ -20,8 +19,6 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
